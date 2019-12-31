5fffc65297c5bfa4b9b2b3887b5372be6e653d9f
Serdar Tumgoren <zstumgoren@gmail.com>
2019-12-31 13:39:16 -0800

    Add GH collaborators in post-gen hook #6

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 from datakit_github.commands import Integrate
+import github
 
 # HACK: Need to direct logging calls to stdout
 # to ensure user prompts display when hook is run
@@ -16,14 +17,31 @@ def get_logger():
     logger.addHandler(handler)
     return logger
 
-create_github_repo = os.environ.get('DKIT_GITHUB_INTEGRATE', True)
-if create_github_repo:
+create_github_repo = os.environ.get('PROGJ_GH_INTEGRATE', 'true')
+if create_github_repo == 'true':
     sys.stdout.write("\n\n~~ Create a Github repo for this project ~~\n\n")
     cmd = Integrate(None, None, cmd_name='github integrate')
     cmd.log = get_logger()
-    cmd.run(argparse.ArgumentParser())
+    integrate_choices = cmd.run(argparse.ArgumentParser())
 
-#TODO: Add instructors as collaborators
+    # Add instructors as collaborators
+    api_key = cmd.configs.get('github_api_key')
+    g = github.Github(api_key)
+    gh_user = integrate_choices['account']
+    repo = g.get_repo("{}/{{ cookiecutter.repo_root }}".format(gh_user))
+    add_collabs = os.environ.get('PROGJ_GH_ADD_COLLABS', 'true')
+    if add_collabs == 'true':
+        try:
+            collaborators = os.environ['PROGJ_GH_COLLABORATORS'].split(':')
+        except KeyError:
+            collaborators = ['zstumgoren', 'dilcia19']
+        for collaborator in collaborators:
+            # Handle when repo owner adds self as collaborator
+            try:
+                repo.add_to_collaborators(collaborator, 'admin')
+            except github.GithubException:
+                pass
+
 sys.stdout.write("\n\nTo wrap up:\n")
 sys.stdout.write("  $ cd {{ cookiecutter.repo_root }}/\n")
 sys.stdout.write("  $ pipenv install\n")
